f2f2f8ec020d11d8d292cf302f4fcd7e88e40ae4
shahin <shahin.shahi1986@gmail.com>
2022-10-05 11:16:16 +0300

    push

--- components/NFTCards.js
+++ components/NFTCards.js
@@ -41,10 +41,10 @@ const NFTCards = ({ nftItem, loadNFTs }) => {
   return (
     <div className="bg-[#303339] w-[15rem] h-[24rem] my-10 mx-5 rounded-2xl overflow-hidden cursor-pointer">
       <div className="h-2/3 w-full overflow-hidden flex justify-center items-center">
-        <Image src={nftItem.image} alt="" />
+        <img src={nftItem.image} alt="" />
       </div>
       <div className="w-full flex justify-center text-white">
-        <Image
+        <img
           className="w-10 h-10 object-cover rounded-full border-2 border-[#202225] mt-[-1rem]"
           src={
             nftItem?.authorImg
@@ -72,7 +72,7 @@ const NFTCards = ({ nftItem, loadNFTs }) => {
           <div className="flex-0.4 text-right">
             <div className="font-semibold text-sm text-[#8a939b]">Price</div>
             <div className="flex items-center text-xl font-bold mt-2">
-              <Image
+              <img
                 src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
                 alt="eth"
                 className="h-5 mr-2"
